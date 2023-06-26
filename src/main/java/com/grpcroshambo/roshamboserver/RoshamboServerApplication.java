082b29ac8db5d546ad39f07ef27b2ab53d3de6bd
Rubeus Hackrid <rhackrid@gmail.com>
2023-06-26 17:06:39 +0200

    init spring boot 2.7.13 project

---
+++ src/main/java/com/grpcroshambo/roshamboserver/RoshamboServerApplication.java
@@ -0,0 +1,13 @@
+package com.grpcroshambo.roshamboserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoshamboServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoshamboServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grpcroshambo/roshamboserver/RoshamboServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grpcroshambo.roshamboserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoshamboServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
